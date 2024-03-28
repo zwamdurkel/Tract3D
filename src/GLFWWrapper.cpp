c9d660b89067693938f8b0bf9ee23c21416295f0
zwamdurkel <github.a@riswick.net>
2024-03-28 18:56:06 +0100

    update GL version to 4.6
    Took 4 hours 9 minutes

--- src/GLFWWrapper.cpp
+++ src/GLFWWrapper.cpp
@@ -14,7 +14,7 @@ void GLFWWrapper::init() {
     settings.glfw = this;
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // for MAC OS
     glfwWindowHint(GLFW_SAMPLES, 4);
